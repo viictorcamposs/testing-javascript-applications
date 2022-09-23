1ec1cc1bf0d6feecac0d3ad0a9522c0132567be0
Victor Campos <victor.campos@escolaconquer.com.br>
2022-09-22 21:19:39 -0300

    implement using TDD method to parse query string to object

--- javascript-testing-introduction/project-01/src/lib/queryString/queryString.js
+++ javascript-testing-introduction/project-01/src/lib/queryString/queryString.js
@@ -1,13 +1,17 @@
 module.exports = {
-  objectToQueryString: obj => {
+  queryString: obj => {
     {
       let result = ''
 
-      Object.entries(obj).forEach(([key, value]) =>
-        result.length <= 0
-          ? (result = `${key}=${value}`)
-          : (result += `&${key}=${value}`),
-      )
+      Object.entries(obj).forEach(([key, value]) => {
+        if (value instanceof Object && !(value instanceof Array)) {
+          throw new Error('Value cannot be an instance of object')
+        } else {
+          return result.length <= 0
+            ? (result = `${key}=${value}`)
+            : (result += `&${key}=${value}`)
+        }
+      })
 
       result = result.replaceAll(' ', '2%')
     }
@@ -20,14 +24,32 @@ module.exports = {
     }
 
     {
+      const keyValueToString = ([key, value]) => {
+        if ((typeof value === 'object') & !Array.isArray(value)) {
+          throw new Error('Value cannot be an instance of object')
+        }
+
+        return `${key}=${value}`
+      }
+
       const result = Object.entries(obj)
-        .map(([key, value]) => {
-          return `${key}=${value}`
-        })
+        .map(keyValueToString)
         .join('&')
         .replaceAll(' ', '2%')
 
       return result
     }
   },
+  parse: queryString =>
+    Object.fromEntries(
+      queryString.split('&').map(query => {
+        let [key, value] = query.split('=')
+
+        if (value.indexOf(',') > -1) {
+          value = value.split(',')
+        }
+
+        return [key, value]
+      }),
+    ),
 }
--- javascript-testing-introduction/project-01/src/lib/queryString/queryString.spec.js
+++ javascript-testing-introduction/project-01/src/lib/queryString/queryString.spec.js
@@ -1,4 +1,4 @@
-const { objectToQueryString } = require('./queryString')
+const { queryString, parse } = require('./queryString')
 
 describe('Object to query string', () => {
   it('should create a valid query string when an object is provided', () => {
@@ -7,14 +7,68 @@ describe('Object to query string', () => {
       profession: 'software developer',
     }
 
-    expect(objectToQueryString(obj)).toBe(
-      'name=Victor&profession=software2%developer',
-    )
+    expect(queryString(obj)).toBe('name=Victor&profession=software2%developer')
 
     obj.company = 'conquer holding'
 
-    expect(objectToQueryString(obj)).toBe(
+    expect(queryString(obj)).toBe(
       'name=Victor&profession=software2%developer&company=conquer2%holding',
     )
+  })
+
+  it('should create a valid query string when an array is provided', () => {
+    const obj = {
+      name: 'Victor',
+      profession: 'software developer',
+      abilities: ['typescript', 'react'],
+    }
+
+    expect(queryString(obj)).toBe(
+      'name=Victor&profession=software2%developer&abilities=typescript,react',
+    )
+  })
+
+  it('should throw an error when an object is passed as value', () => {
+    const obj = {
+      name: 'Victor',
+      abilities: {
+        first: 'typescript',
+        second: 'react',
+      },
+    }
+
+    expect(() => queryString(obj)).toThrowError()
+  })
+
+  it('should convert a query string to object', () => {
+    const qS = 'name=Victor&profession=developer'
+
+    const result = {
+      name: 'Victor',
+      profession: 'developer',
+    }
+
+    expect(parse(qS)).toEqual(result)
+  })
+
+  it('should convert a query string of a single key-value to object', () => {
+    const qs = 'name=Victor'
+
+    const result = {
+      name: 'Victor',
+    }
+
+    expect(parse(qs)).toEqual(result)
+  })
+
+  it('should convert a query string to an object taking care of comma separated values', () => {
+    const qs = 'name=Victor,Campos&abilities=typescript,react'
+
+    const result = {
+      name: ['Victor', 'Campos'],
+      abilities: ['typescript', 'react'],
+    }
+
+    expect(parse(qs)).toEqual(result)
   })
 })
