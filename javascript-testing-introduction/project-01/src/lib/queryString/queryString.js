586eb9c30d86c5b3c0ea7358a69510d83a4863d1
Victor Campos <victor.campos@escolaconquer.com.br>
2022-09-24 15:07:45 -0300

    convert commonjs to modules and intro to babel

--- javascript-testing-introduction/project-01/src/lib/queryString/queryString.js
+++ javascript-testing-introduction/project-01/src/lib/queryString/queryString.js
@@ -1,53 +1,49 @@
-module.exports = {
-  queryString: obj => {
-    // {
-    //   let result = ''
-
-    //   Object.entries(obj).forEach(([key, value]) => {
-    //     if (value instanceof Object && !(value instanceof Array)) {
-    //       throw new Error('Value cannot be an instance of object')
-    //     } else {
-    //       return result.length <= 0
-    //         ? (result = `${key}=${value}`)
-    //         : (result += `&${key}=${value}`)
-    //     }
-    //   })
-
-    //   result = result.replaceAll(' ', '2%')
-    // }
-
-    // {
-    //   const result = Object.entries(obj)
-    //     .join('&')
-    //     .replaceAll(',', '=')
-    //     .replaceAll(' ', '2%')
-    // }
-
-    const keyValueToString = ([key, value]) => {
-      if (typeof value === 'object' && !Array.isArray(value)) {
-        throw new Error('Value cannot be an instance of object')
-      }
-
-      return `${key}=${value}`
+export function queryString(obj) {
+  // {
+  //   let result = ''
+  //   Object.entries(obj).forEach(([key, value]) => {
+  //     if (value instanceof Object && !(value instanceof Array)) {
+  //       throw new Error('Value cannot be an instance of object')
+  //     } else {
+  //       return result.length <= 0
+  //         ? (result = `${key}=${value}`)
+  //         : (result += `&${key}=${value}`)
+  //     }
+  //   })
+  //   result = result.replaceAll(' ', '2%')
+  // }
+  // {
+  //   const result = Object.entries(obj)
+  //     .join('&')
+  //     .replaceAll(',', '=')
+  //     .replaceAll(' ', '2%')
+  // }
+  const keyValueToString = ([key, value]) => {
+    if (typeof value === 'object' && !Array.isArray(value)) {
+      throw new Error('Value cannot be an instance of object')
     }
 
-    const result = Object.entries(obj)
-      .map(keyValueToString)
-      .join('&')
-      .replaceAll(' ', '2%')
+    return `${key}=${value}`
+  }
 
-    return result
-  },
-  parse: queryString =>
-    Object.fromEntries(
-      queryString.split('&').map(query => {
-        let [key, value] = query.split('=')
+  const result = Object.entries(obj)
+    .map(keyValueToString)
+    .join('&')
+    .replaceAll(' ', '2%')
 
-        if (value.indexOf(',') > -1) {
-          value = value.split(',')
-        }
+  return result
+}
 
-        return [key, value]
-      }),
-    ),
+export function parse(queryString) {
+  return Object.fromEntries(
+    queryString.split('&').map(query => {
+      let [key, value] = query.split('=')
+
+      if (value.indexOf(',') > -1) {
+        value = value.split(',')
+      }
+
+      return [key, value]
+    }),
+  )
 }
