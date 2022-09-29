4c77176cd547bfbbec67c4ce82de336bd70c4f76
Victor Campos <campos.viictor@gmail.com>
2022-09-28 23:34:14 -0300

    init project "testing-react-next-application"

---
+++ testing-react-next-application/src/pages/index.jsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <h1 className="text-center border-1 mt-24 text-6xl">🚀</h1>
+}
+
+export default Home
---
+++ testing-react-next-application/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'accent-1': '#333',
+      },
+    },
+  },
+  plugins: [],
+}
