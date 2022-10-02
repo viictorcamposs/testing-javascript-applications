b06db25211856b935d9f8049a75f27f03bf4f911
Victor Campos <campos.viictor@gmail.com>
2022-10-02 14:22:43 -0300

    add test coverage config

--- testing-react-next-application/jest.config.js
+++ testing-react-next-application/jest.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   setupFilesAfterEnv: ['<rootDir>/setupTests.js/'],
+  collectCoverageFrom: [
+    '<rootDir>/src/components/**/*.(js|jsx)',
+    '<rootDir>/src/pages/**/*.(js|jsx)',
+  ],
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
   },
